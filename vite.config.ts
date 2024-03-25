0abafb303f9ae87403b2a646d0702ab183e54fe2
Gogo200202 <gosheto420@gmail.com>
2024-03-25 18:48:59 +0200

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://gogo200202.github.io/My_dogy_site/",
   plugins: [react()],
 })
